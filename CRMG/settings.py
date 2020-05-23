596d273bafee1e75a446a4009a62b7594aa80892
Welbert Fernandes Costa <welbert.jsj@gmail.com>
2020-05-23 00:26:38 -0300

    settings heroku ps

--- CRMG/settings.py
+++ CRMG/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,3 +146,5 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10
 }
+
+django_heroku.settings(locals())
