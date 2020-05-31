2ca11cbfa0989d6cdd46dcc909ec543d7449b8cc
Welbert Fernandes Costa <welbert.jsj@gmail.com>
2020-05-31 01:17:39 -0300

    testelogin3

--- entidades/serializers.py
+++ entidades/serializers.py
@@ -108,6 +108,41 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 class LoginEscolaSerializer(serializers.ModelSerializer):
     user = CharField(required=False, allow_blank=True)
     senha = CharField(required=False, allow_blank=True)
+    id = CharField(required=False, allow_blank=True)
+    class Meta:
+        model = Escola
+        fields = (
+            'id',
+            'user',
+            'senha'
+        )
+
+    def validate(self, data):
+        user = data.get("user", None)
+        senha = data.get("senha", None)
+        id = data.get("id", None)
+
+        if not senha:
+            raise ValidationError("senha não encontrado")
+
+        validacao = Escola.objects.filter(
+            Q(user=user)|
+            Q(senha=senha)
+        )
+        
+
+        if validacao.exists() and validacao.count() == 1:
+            validacao = validacao.first()
+        else:
+            raise ValidationError("Usuario ou senha incorreto")
+
+        return data
+
+
+'''
+class LoginEscolaSerializer(serializers.ModelSerializer):
+    user = CharField(required=False, allow_blank=True)
+    senha = CharField(required=False, allow_blank=True)
     class Meta:
         model = Escola
         fields = (
@@ -134,6 +169,8 @@ class LoginEscolaSerializer(serializers.ModelSerializer):
         else:
             raise ValidationError("Usuario ou senha incorreto")
 
-        return data
+        return dat
+
+'''
 
 
