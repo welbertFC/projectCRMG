1388eb08f540c851eb56c44ba4d533d1948387a0
Welbert Fernandes Costa <welbert.jsj@gmail.com>
2020-06-28 00:53:32 -0300

    apendices

--- entidades/views.py
+++ entidades/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST 
