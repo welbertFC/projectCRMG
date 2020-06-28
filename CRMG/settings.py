63ede2df41ee78a9817d6175a36273cf2717d3e5
Welbert Fernandes Costa <welbert.jsj@gmail.com>
2020-06-28 01:10:11 -0300

    apendices

--- CRMG/settings.py
+++ CRMG/settings.py
@@ -156,7 +156,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10
