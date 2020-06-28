e720cd12bbbf38867defbe8e84e4f6090639de9c
Welbert Fernandes Costa <welbert.jsj@gmail.com>
2020-06-27 23:11:42 -0300

    login

--- entidades/serializers.py
+++ entidades/serializers.py
@@ -140,7 +140,7 @@ class LoginEscolaSerializer(serializers.ModelSerializer):
         else:
             raise ValidationError("Usuario ou senha incorreto")
 
-        data['id'] = Escola.objects.filter(id=id)
+        data['id'] = Escola.objects.filter(Q(id=id))
 
         return data
     
