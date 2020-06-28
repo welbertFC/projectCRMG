0486d8ce9b64321da873dd6c88727e8a59f6caa7
Welbert Fernandes Costa <welbert.jsj@gmail.com>
2020-06-27 21:56:25 -0300

    login

--- entidades/serializers.py
+++ entidades/serializers.py
@@ -129,10 +129,10 @@ class LoginEscolaSerializer(serializers.ModelSerializer):
             Q(senha=senha),
         ) 
 
-        escola_pk = Escola.objects.get(pk)
+        escola_pk = Escola.object.get(pk)
 
         if validacao.exists() and validacao.count() == 1:
-            validacao = validacao.first()
+            #validacao = validacao.first()
             escola_pk
 
         else:
